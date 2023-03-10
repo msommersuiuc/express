275d789c74c8c47f193d6d4b7f243195e31d460d
UOFI\msommers <msommers@illinois.edu>
2023-03-09 20:17:52 -0600

    inital

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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"
+});
+const openai = new OpenAIApi(configuration);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post("/submit", async (req, res, next) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      temperature: 0,
+      max_tokens: 7,
+    });
+    
+    res.json(response.data);
+  } catch {
+    res.json(false);
+  }
+});
+module.exports = router;
